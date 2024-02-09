4b28090c2f04867097f553f38cc21969a12d62d4
Neil Carpenter <random.tangent@gmail.com>
2024-02-09 13:58:21 -0500

    public

--- main.tf
+++ main.tf
@@ -13,3 +13,10 @@ provider "aws" {
 resource "aws_s3_bucket" "s3" {
   bucket = "terraform-experiments"
 }
+
+resource "aws_s3_bucket_public_access_block" "s3" {
+  bucket = aws_s3_bucket.s3.id
+
+  block_public_acls   = false
+  block_public_policy = false
+}
