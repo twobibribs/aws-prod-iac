86c764625f86fbb2e5e5d3dae7dffb4cf3db230d
Neil Carpenter <random.tangent@gmail.com>
2024-02-09 12:35:41 -0500

    Fix site

--- variables.tf
+++ variables.tf
@@ -1,39 +1,85 @@
- # Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- # SPDX-License-Identifier: MIT-0
- #
- # Permission is hereby granted, free of charge, to any person obtaining a copy of this
- # software and associated documentation files (the "Software"), to deal in the Software
- # without restriction, including without limitation the rights to use, copy, modify,
- # merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- # permit persons to whom the Software is furnished to do so.
- #
- # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- # INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- # PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- # HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
-#################################
-## waf-conditions.tf Variables ##
-#################################
-variable "AWS_Security_Blog_Blacklist_IPSet_Name" {
-  default = "devsecopsblacklistipset"
-}
-variable "AWS_Security_Blog_SQLI_Match_Set_Name" {
-  default = "devsecopssqlimatchset"
+variable "site_domain" {
+  description = "Domain on which the static site will be made available (e.g. `\"www.example.com\"`)"
 }
 
-############################
-## waf-rules.tf Variables ##
-############################
-variable "AWS_Security_Blog_IP_Blacklist_Rule_Name" {
-  default = "devsecopsblacklistrule"
+variable "name_prefix" {
+  description = "Name prefix to use for objects that need to be created (only lowercase alphanumeric characters and hyphens allowed, for S3 bucket name compatibility)"
+  default     = "aws-static-site---"
 }
-variable "AWS_Security_Blog_SQL_Injection_Rule_Name" {
-  default = "devsecopssqlirule"
+
+variable "comment_prefix" {
+  description = "This will be included in comments for resources that are created"
+  default     = "Static site: "
 }
-variable "AWS_Security_Blog_Blacklist_WACL_Name" {
-  default = "devsecopswebacl"
+
+variable "bucket_override_name" {
+  description = "When provided, assume a bucket with this name already exists for the site content, instead of creating the bucket automatically (e.g. `\"my-bucket\"`)"
+  default     = ""
+}
+
+variable "cloudfront_price_class" {
+  description = "CloudFront price class to use (`100`, `200` or `\"All\"`, see https://aws.amazon.com/cloudfront/pricing/)"
+  default     = 100
+}
+
+variable "viewer_https_only" {
+  description = "Set this to `false` if you need to support insecure HTTP access for clients, in addition to HTTPS"
+  default     = true
+}
+
+variable "cache_ttl_override" {
+  description = "When >= 0, override the cache behaviour for ALL objects in S3, so that they stay in the CloudFront cache for this amount of seconds"
+  default     = -1
+}
+
+variable "default_root_object" {
+  description = "The object to return when the root URL is requested"
+  default     = "index.html"
+}
+
+variable "add_response_headers" {
+  description = "Map of HTTP headers (if any) to add to outgoing responses before sending them to clients"
+  type        = "map"
+
+  default = {
+    "Strict-Transport-Security" = "max-age=31557600; preload" # i.e. 1 year (in seconds)
+  }
+}
+
+variable "basic_auth_username" {
+  description = "When non-empty, require this username with HTTP Basic Auth"
+  default     = ""
+}
+
+variable "basic_auth_password" {
+  description = "When non-empty, require this password with HTTP Basic Auth"
+  default     = ""
+}
+
+variable "basic_auth_realm" {
+  description = "When using HTTP Basic Auth, this will be displayed by the browser in the auth prompt"
+  default     = "Authentication Required"
+}
+
+variable "basic_auth_body" {
+  description = "When using HTTP Basic Auth, and authentication has failed, this will be displayed by the browser as the page content"
+  default     = "Unauthorized"
+}
+
+variable "lambda_logging_enabled" {
+  description = "When true, writes information about incoming requests to the Lambda function's CloudWatch group"
+  default     = false
+}
+
+variable "tags" {
+  description = "AWS Tags to add to all resources created (where possible); see https://aws.amazon.com/answers/account-management/aws-tagging-strategies/"
+  type        = "map"
+  default     = {}
+}
+
+locals {
+  prefix_with_domain = "${var.name_prefix}${replace("${var.site_domain}", "/[^a-z0-9-]+/", "-")}"                          # only lowercase alphanumeric characters and hyphens are allowed in S3 bucket names
+  bucket_name        = "${var.bucket_override_name == "" ? "${local.prefix_with_domain}" : "${var.bucket_override_name}"}" # select between externally-provided or auto-generated bucket names
+  bucket_domain_name = "${local.bucket_name}.s3-website.${data.aws_region.current.name}.amazonaws.com"                     # use current region to complete the domain name (we can't use the "aws_s3_bucket" data source because the bucket may not initially exist)
+  error_ttl          = "${var.cache_ttl_override >= 0 ? var.cache_ttl_override : 0}"
 }
